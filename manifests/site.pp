2f74136de25fc4af2176ba31b2ffb23b54ce5989
jnorrisatminesdotedu <jnorris@mines.edu>
2022-10-26 15:48:06 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme line1',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
 }
